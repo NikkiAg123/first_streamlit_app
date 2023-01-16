468a5a00d25eba97b9a28a64aac5e70be6cb2f63
Nikki Agarwal <122526859+NikkiAg123@users.noreply.github.com>
2023-01-16 22:03:01 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,6 +5,6 @@ streamlit.title('My Parents New Healthy Dinner')
 
 streamlit.header('Breakfast Menu')
 
-streamlit.text('Oatmeal')
-streamlit.text('Sandwitch')
-streamlit.text('Juice')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
